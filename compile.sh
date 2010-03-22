54e0fbf5af9553c183be795943152e1f313757ad
gapan <gapan@a8afeb8c-277c-4b4a-a4f6-ef4c85adc32b>
2010-03-22 22:30:09 +0000

    Renamed locale files to LOCALE.po for transifex

--- compile.sh
+++ compile.sh
@@ -2,5 +2,5 @@
 
 for i in `ls locale/*.po`;do
 	echo "Compiling `echo $i|sed "s|locale/||"`"
-	msgfmt $i -o `echo $i |sed "s/salix-codecs-installer-//"|sed "s/.po//"`.mo
+	msgfmt $i -o `echo $i |sed "s/.po//"`.mo
 done
