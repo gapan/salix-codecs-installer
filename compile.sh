ee50b57dd7a8a2ef6897dcc189f7d06ca43a7195
gapan <gapan@a8afeb8c-277c-4b4a-a4f6-ef4c85adc32b>
2010-03-23 19:30:48 +0000

    Updated compile scripts

--- compile.sh
+++ compile.sh
@@ -4,3 +4,6 @@ for i in `ls po/*.po`;do
 	echo "Compiling `echo $i|sed "s|po/||"`"
 	msgfmt $i -o `echo $i |sed "s/.po//"`.mo
 done
+
+intltool-merge po/ -d -u salix-codecs-installer.desktop.in salix-codecs-installer.desktop
+intltool-merge po/ -d -u salix-codecs-installer-kde.desktop.in salix-codecs-installer-kde.desktop
