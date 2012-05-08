9841335952eab818f57de02884689dc15f518edc
gapan <gapan@a8afeb8c-277c-4b4a-a4f6-ef4c85adc32b>
2012-05-08 20:26:21 +0000

    Cleaned up trunk

--- compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for i in `ls po/*.po`;do
-	echo "Compiling `echo $i|sed "s|po/||"`"
-	msgfmt $i -o `echo $i |sed "s/.po//"`.mo
-done
-
-intltool-merge po/ -d -u salix-codecs-installer.desktop.in salix-codecs-installer.desktop
-intltool-merge po/ -d -u salix-codecs-installer-kde.desktop.in salix-codecs-installer-kde.desktop
